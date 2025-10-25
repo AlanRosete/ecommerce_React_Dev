908e10912c59e1e97432a3a143ed168a4c71a954
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2025-10-25 11:34:27 -0600

    Icon Hamburger Animate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,27 +41,24 @@ function App({ navigationItems }) {
                 <div className="block lg:hidden">
                     <button
                         onClick={() => setIsOpen(!isOpen)}
-                        className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+                        className={`relative w-8 h-6 flex flex-col justify-between items-center group transition-all duration-300`}
                     >
-                        <svg
-                            className={`fill-current h-6 w-6 ${
-                                isOpen ? "hidden" : "block"
-                            }`}
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                        </svg>
-                        <svg
-                            className={`fill-current h-6 w-6 ${
-                                isOpen ? "block" : "hidden"
-                            }`}
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-                        </svg>
-                    </button>
+                    <span
+                    className={`block h-[3px] w-full rounded bg-black transform transition-all duration-300 ease-in-out ${
+                        isOpen ? 'rotate-45 translate-y-[12px]' : ''
+                    }`}
+                    ></span>
+                    <span
+                    className={`block h-[3px] w-full rounded bg-black transition-all duration-300 ease-in-out ${
+                        isOpen ? 'opacity-0' : 'opacity-100'
+                    }`}
+                    ></span>
+                    <span
+                    className={`block h-[3px] w-full rounded bg-black transform transition-all duration-300 ease-in-out ${
+                        isOpen ? '-rotate-45 -translate-y-[9px]' : ''
+                    }`}
+                    ></span>
+                </button>
                 </div>
 
                 {/* navigation links */}
