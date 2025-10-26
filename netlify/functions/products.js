75855d7e5841bb484e09950470b4c2e54e0bc348
Alan Rosete <78501259+AlanRosete@users.noreply.github.com>
2025-10-26 16:16:39 -0600

    Clean ENV Deployd API

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,23 +1,22 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export const handler = async () => {
-  try {
-    const { data, error } = await supabase.from('products').select('*');
-    if (error) throw error;
+  const { data, error } = await supabase.from('products').select('*');
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (err) {
+  if (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
+      body: JSON.stringify({ error: error.message }),
     };
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 };
